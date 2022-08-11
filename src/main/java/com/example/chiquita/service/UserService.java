0123769c14c60c50adeb3363a6a6be5f04b86e7d
Katerina Vratilova <katerina.vratilova@gmail.com>
2022-08-11 21:47:42 +0200

    Fixed mistakes in UserService class

--- src/main/java/com/example/chiquita/service/UserService.java
+++ src/main/java/com/example/chiquita/service/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -37,19 +38,24 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
-    @Transactional
-    public UserEntity saveUser(RegisterRequest registerRequest) {
-        var userEntity = new UserEntity();
-//        BeanUtils.copyProperties(registerRequest, userEntity);
-        userEntity.setEmail(registerRequest.email());
-        userEntity.setActive(true);
-        userEntity.setFirstName(registerRequest.firstName());
-        userEntity.setLastName(registerRequest.lastName());
-        userEntity.setPassword(passwordEncoder.encode(registerRequest.password()));
+  @Transactional
+  public UserEntity saveUser(RegisterRequest registerRequest) {
+    UserEntity user = new UserEntity();
 
-        var roleEntity = roleRepository.findByRole("USER_BASIC");
-        userEntity.setRoles(new HashSet<RoleEntity>(Collections.singletonList(roleEntity)));
+    user.setEmail(registerRequest.email());
+    user.setActive(true);
+    user.setFirstName(registerRequest.firstName());
+    user.setLastName(registerRequest.lastName());
+    user.setPassword(passwordEncoder.encode(registerRequest.password()));
 
-        return userRepository.save(userEntity);
-    }
+    RoleEntity userRole = roleRepository.findByRole("USER_BASIC");
+    user.setRoles(new HashSet<RoleEntity>(Collections.singletonList(userRole)));
+
+    return userRepository.save(user);
+  }
+
+  public UserEntity getById(UUID id) throws Exception {
+    return userRepository.findById(id)
+      .orElseThrow(() -> new Exception(String.format("Cannot find [%s] user", id)));
+  }
 }
